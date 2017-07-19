2ebe345275cfe1d29d726550ac8791c043fe299c
sideproject <sideproject>
2017-07-18 23:14:21 -0400

    intit

---
+++ nav-flash-1.1.0/nav-flash-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "nav-flash" "1.1.0" "Briefly highlight the current line" 'nil :commit "9054a0f9b51da9e5207672efc029ba265ba28f34" :url "http://github.com/rolandwalker/nav-flash" :keywords '("extensions" "navigation" "interface"))
